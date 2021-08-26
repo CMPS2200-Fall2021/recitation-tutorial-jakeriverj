fde7be0b4269607e5e44f43f2d2ef6c0340bd03a
jakeriverj <77644658+jakeriverj@users.noreply.github.com>
2021-08-26 10:10:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def sum_of_squares(a):
-	pass
+	answer = 0
+	for x in a:
+		answer += (x*x)
+	return answer
 
 def test_one():
-    assert sum_of_squares([1,2,3]) == 14
+    assert sum_of_squares([1,2,3]) == 14, "False"
+
+def test_two():
+	assert sum_of_squares([1,2,3,4]) == 30, "False"
+
+test_one()
+test_two()
